f93cf465160c63e7c31dd003984498706cbdd7d6
StasRyabokon <ya.stas.rybokon@gmail.com>
2018-07-16 16:48:08 +0300

    add controller

---
+++ src/main/java/com/spring/user/controller/UserController.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.spring.user.controller;
+
+import com.spring.user.model.User;
+import com.spring.user.service.UserServiceImpl;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author stas
+ */
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserServiceImpl service;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return service.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable Long id) {
+        return service.getUser(id);
+    }
+
+    @PostMapping
+    public User addUser(@RequestBody User user) {
+        return service.addUser(user);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User user) {
+        return service.updateUser(id, user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        service.deleteUser(id);
+    }
+
+}
---
+++ src/main/resources/data-h2.sql
@@ -0,0 +1,3 @@
+INSERT INTO `user`(id, firstname, lastname, birth, login, password, about, address, email)
+VALUES(1,'Stas','Ryabokon','1997-06-05','Stasyany4','NotMyActualPassword','ambitious promising Java developer', 'Nizhynska St, 29B, Kyiv, 02000','ya.stas.rybokon@gmail.com')
+
